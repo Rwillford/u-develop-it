c8b857abf69da6b2d057f32978eb2bceb014c526
Bob <rwillford0719@gmail.com>
2022-05-21 10:29:36 -0500

    added parties to sql and server

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const db = mysql.createConnection(
 
 //GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if(err){
@@ -38,7 +42,12 @@ app.get('/api/candidates', (req, res) => {
 
 //GET a single candidate
 app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id]
     
     db.query(sql, params, (err, row) => {
@@ -83,22 +92,21 @@ app.post('/api/candidate', ({ body }, res) => {
         res.status(400).json({errpr: errors})
         return;
     }
-})
-
-const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
     VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
-
-db.query(sql, params, (err, result) => {
-    if(err){
-        res.status(400).json({error: err.message});
-        return;
-    }
-    res.json({
-        message: 'success',
-        data: body
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })
     })
-    console.log(result)
 })
 
 //default response for any other request (Not Found)
